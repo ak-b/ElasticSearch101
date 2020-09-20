e2f581a721b1e67a3418c888ecc071f63cb678a0
Akansha <36146495+ak-b@users.noreply.github.com>
2020-09-20 18:58:54 +0530

    Update kibana_logs.py

--- kibana_logs.py
+++ kibana_logs.py
@@ -1,15 +1,5 @@
 """
-This python script IS a ROUGH DRAFT to showcase the use case of extracting embryonic connection logs when
-the PER-CLIENT HOST LIMIT is breached
-Input: Start and End Date a.k.a TIMESTAMPS and GEOLOCATION a.k.a DC:SJC,DFW,YYZ,JFK,IAD,LHR,AMS,SIN,NRT,SYD
-Output: Top 2 talkers with specification for 
-(a)log source
-(b)timestamp
-(grouped) message : c,d,e,f
-(c)source IP and port
-(d)destination IP and port
-(d)embryonic connection limit breached
-(f)number of clients connected 
+This python script IS a ROUGH DRAFT to showcase the use case of kibana logs
 """
 
 import json,requests
@@ -23,7 +13,6 @@ from tabulate import tabulate
 
 def connect_elasticsearch():
     _es = None
-    #_es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
     if _es.ping():
         print('Yay Connect')
     else:
@@ -37,10 +26,10 @@ def search():
     Add a geolocation map for CLP search URL
     Provide user input params for (A) timestamp (B) GEOLOCATION/DC
     """
-    url = "https://clpsj-infra.webex.com/esapi/*-infra-*/_search"
+    url = "xxx"
     payload = "{\n  \"version\": true,\n  \"size\": 2,\n  \"sort\": [\n    {\n      \"@timestamp\": {\n        \"order\": \"desc\",\n        \"unmapped_type\": \"boolean\"\n      }\n    }\n  ],\n  \"_source\": {\n    \"excludes\": []\n  },\n  \"aggs\": {\n    \"2\": {\n      \"date_histogram\": {\n        \"field\": \"@timestamp\",\n        \"interval\": \"3h\",\n        \"time_zone\": \"UTC\",\n        \"min_doc_count\": 1\n      }\n    }\n  },\n  \"stored_fields\": [\n    \"*\"\n  ],\n  \"script_fields\": {},\n  \"docvalue_fields\": [\n    {\n      \"field\": \"@timestamp\",\n      \"format\": \"date_time\"\n    }\n  ],\n  \"query\": {\n    \"bool\": {\n      \"must\": [\n        {\n          \"query_string\": {\n            \"query\": \"\\\"Embryonic\\\"\",\n            \"analyze_wildcard\": true,\n            \"default_field\": \"*\"\n          }\n        },\n        {\n          \"range\": {\n            \"@timestamp\": {\n              \"gte\": 1599045724923,\n              \"lte\": 1599650524923,\n              \"format\": \"epoch_millis\"\n            }\n          }\n        }\n      ],\n      \"filter\": [],\n      \"should\": [],\n      \"must_not\": []\n    }\n  },\n  \"highlight\": {\n    \"pre_tags\": [\n      \"@kibana-highlighted-field@\"\n    ],\n    \"post_tags\": [\n      \"@/kibana-highlighted-field@\"\n    ],\n    \"fields\": {\n      \"*\": {}\n    },\n    \"fragment_size\": 2147483647\n  }\n}"
     headers = {
-	  'Authorization': 'Basic YWtiYW5zYWw6TmV3c2Nvb3RlcjEyM0A=',
+	  'Authorization': 'Basic xxxxx',
 	  'Content-Type': 'application/json'
     }
 
@@ -48,10 +37,10 @@ def search():
     results = json.loads(response.text)
     print(" Kibana Embryonic Connections Logs")
     for i in range(2):
-	    result_a = results['hits']['hits'][i]['_source']['logsource']
-	    result_b = results['hits']['hits'][i]['_source']['@timestamp']
-	    result_c = results['hits']['hits'][i]['_source']['message']
-	    result_x = results['hits']['hits'][i]['_source']['host']
+	    result_a = results['hits']['hits'][i]['_source']['']
+	    result_b = results['hits']['hits'][i]['_source']['']
+	    result_c = results['hits']['hits'][i]['_source']['']
+	    result_x = results['hits']['hits'][i]['_source']['']
 	    client_perhost = str(re.findall(r"\S+\d+?[0]",result_c)[1])
 	    srcip_port=str(re.findall(r"\S+\d+?[1]",result_c)[1])
 	    destip_port= str(re.findall(r"\S+\d+?[3]",result_c)[1])
